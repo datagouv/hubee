2693d25a116ffdaddc8741ab2330921d1a8e88dd
Damien Le Thiec <damien@dlet.fr>
2025-10-31 17:18:53 +0100

    fix: remove duplicate SIRET index migration
      - Deleted duplicate migration adding siret index
      - Index already created in CreateOrganizations
      - Regenerated schema after database reset

--- db/migrate/20251028004632_add_unique_siret_index_to_organizations.rb
+++
@@ -1,7 +0,0 @@
-class AddUniqueSiretIndexToOrganizations < ActiveRecord::Migration[8.1]
-  disable_ddl_transaction!
-
-  def change
-    add_index :organizations, :siret, unique: true, algorithm: :concurrently
-  end
-end
