4017478376f17ec88edca16deb25f8b99dece33e
Julien ANNE <contact@julien-anne.fr>
2026-06-18 20:09:25 +0200

    feat(portail): bootstrapper la structure ::Portail
    Crée le namespace ::Portail minimal pour démarrer le développement
    du portail V2 de façon isolée de ::API.
    
    - Portail::BaseController (layout dédié) + DashboardController
    - Route GET /portail → portail/dashboard#index
    - Spec request + feature Cucumber smoke test

---
+++ app/controllers/portail/base_controller.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Portail
+  class BaseController < ApplicationController
+    layout "portail"
+  end
+end
---
+++ app/controllers/portail/dashboard_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module Portail
+  class DashboardController < Portail::BaseController
+    def index
+    end
+  end
+end
